99e4be7f2f8c21c429e0d117618272f7626c48de
Ana Carolina <anamachadodev@gmail.com>
2025-07-30 09:06:55 -0300

    Adiconando o provider DB

--- lib/data/app_database.dart
+++ lib/data/app_database.dart
@@ -1,29 +1,37 @@
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as p;
+import 'package:drift_flutter/drift_flutter.dart';
 import 'package:to_do_list/data/tables/category_table.dart';
 import 'package:to_do_list/data/tables/project_table.dart';
-import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'tables/task_table.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(
-  tables: [Tasks, Categories, Projects],
-)
+// Esse é o DB provider
+@Riverpod(keepAlive: true)
+AppDatabase appDatabase(Ref ref) {
+  final db = AppDatabase();
+
+  ref.onDispose(() {
+    db.close();
+  });
+
+  return db;
+}
+
+// É a classe main do db
+@DriftDatabase(tables: [Tasks, Categories, Projects])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(_openConnection());
 
+  static QueryExecutor _openConnection() {
+    return driftDatabase(
+      name: 'to_do_list_app',
+    );
+  }
+
   @override
   int get schemaVersion => 1;
-}
-
-LazyDatabase _openConnection() {
-  return LazyDatabase(() async {
-    final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'app.db'));
-    return NativeDatabase(file);
-  });
 }
--- lib/data/app_database.g.dart
+++ lib/data/app_database.g.dart
@@ -2095,3 +2095,26 @@ class $AppDatabaseManager {
   $$ProjectsTableTableManager get projects =>
       $$ProjectsTableTableManager(_db, _db.projects);
 }
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appDatabaseHash() => r'67f06207fff3a55949c4c4b67200f868a9b6acc8';
+
+/// See also [appDatabase].
+@ProviderFor(appDatabase)
+final appDatabaseProvider = Provider<AppDatabase>.internal(
+  appDatabase,
+  name: r'appDatabaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appDatabaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AppDatabaseRef = ProviderRef<AppDatabase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
